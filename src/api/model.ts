d653b77cad3c4af22a85c8bceb22e9bb0e6c74ef
kurous <wx2178@126.com>
2025-11-16 18:28:40 +0800

    feature: 验证fetch，并成功接入后端

---
+++ src/api/model.ts
@@ -0,0 +1,17 @@
+/**
+ * @Author: kurous wx2178@126.com
+ * @Date: 2025-11-16 18:19:00
+ * @LastEditors: kurous wx2178@126.com
+ * @LastEditTime: 2025-11-16 18:21:12
+ * @FilePath: src/api/model.ts
+ * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+ */
+
+export interface GameWebViewRsp {
+  id: number;
+  name: string;
+  type: number;
+  type_name: string;
+  created_at: string;
+  updated_at: string;
+}
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -2,13 +2,24 @@
  * @Author: kurous wx2178@126.com
  * @Date: 2025-11-16 16:30:54
  * @LastEditors: kurous wx2178@126.com
- * @LastEditTime: 2025-11-16 17:41:23
+ * @LastEditTime: 2025-11-16 18:23:48
  * @FilePath: src/app/game/page.tsx
  * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
  */
+'use client';
+import {listGames, ping} from "@/api/ping";
+
 const EventsPage = () => {
 
   const events = ['马拉松大赛', '半程跑步比赛', '城市慢跑'] as const;
+
+  async function handleEventClick() {
+    let response = await ping();
+    console.log('Ping response:', response);
+
+    response = await listGames();
+    console.log('List Games response:', response);
+  }
 
   return (
     <div className="p-8">
@@ -16,7 +27,7 @@ const EventsPage = () => {
       <ul>
         {events.map((event) => (
           <li key={event} className="cursor-pointer mb-2 hover:text-blue-600">
-            <button>
+            <button onClick={handleEventClick}>
               {event}
             </button>
           </li>
