4f936bd98bdfd9a6cc54cda6db6d3ce6ee1c3af7
kurous <wx2178@126.com>
2025-11-16 11:29:35 +0800

    feature: 增加tailwindcss daisyui，以及主题切换组件

---
+++ postcss.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @Author: kurous wx2178@126.com
+ * @Date: 2025-11-16 10:16:57
+ * @LastEditors: kurous wx2178@126.com
+ * @LastEditTime: 2025-11-16 10:17:22
+ * @FilePath: postcss.config.mjs
+ * @Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+ */
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
+export default config;
