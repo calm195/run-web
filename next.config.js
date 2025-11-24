d789572ee4a2d03de1fde6d28312f96fdf0142c0
kurous <wx2178@126.com>
2025-11-24 19:50:21 +0800

    next降级14.2.33

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 你的配置，例如：
+  // output: 'export',
+  // images: { domains: ['example.com'] },
+};
+
+module.exports = nextConfig;
